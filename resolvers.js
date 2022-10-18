0b9bb9ce814c97cf3a4ad5a38e175e68e6b03603
km1220 <km6576803@gmail.com>
2022-10-18 07:06:00 +0200

    2: modify: get fullName by Students.root.firstName & lastName

--- resolvers.js
+++ resolvers.js
@@ -5,7 +5,20 @@ const Query = {
 	students: () => db.students.list(),
 
 	studentById: (root, args, context, info) => {
+		// In this case, [root] is "null"
+		console.log('========== studentById ========== : ', root, args, context, info)
+
 		return db.students.get(args.id);
 	}
 }
-module.exports = { Query };
+
+const Students = {
+	fullName: (root, args, context, info) => {
+		// In this case, [args] is "null"
+		console.log('========== fullName ========== : ', root, args, context, info)
+
+		return root.firstName + " : " + root.lastName
+	}
+}
+
+module.exports = { Query, Students };
