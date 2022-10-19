ace8a8bfaa8ecd4a81932023076a432e2bc8db77
km1220 <km6576803@gmail.com>
2022-10-19 08:01:27 +0200

    8: add: query validation

--- resolvers.js
+++ resolvers.js
@@ -60,7 +60,36 @@ const Mutation = {
 			lastName: args.lastName
 		})
 		return db.students.get(new_id)
+	},
+
+
+
+
+
+
+
+
+
+
+
+
+	signUp: (root, args, context, info) => {
+		const { email, firstName, password } = args.input;
+		const emailExpression =
+			// /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		const isValidEmail = emailExpression.test(String(email).toLowerCase())
+
+		if (!isValidEmail)
+			throw new Error("email not in proper format")
+		if (firstName.length > 15)
+			throw new Error("firstName should be less than 15 characters")
+		if (password.length < 8)
+			throw new Error("password should be minimum 8 characters")
+		return "success";
 	}
+
+
 }
 
 module.exports = {
